6d4f1ba5ba0ab35ff0690a0107fca2077a72d890
胡善清 <shanqing.hu@casstime.com>
2023-12-25 18:04:06 +0800

    feat: 初始化Editor-2

---
+++ demos/Basic.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Editor } from 'tiny-image-editor';
+
+const Demo = () => {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+};
+
+export default Demo;
---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+hero:
+  title: tiny-image-editor
+  description: A react image editor of fabricjs
+features:
+---
+
+### 测试
+
+<code src="../demos/Basic.tsx"></code>
